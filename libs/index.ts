3379475079879ced21bc632eb654114fec148567
Hakan Altındiş <HakanA@d-teknoloji.com.tr>
2023-01-16 12:40:16 +0300

    - Exported HttpResponse interface

--- libs/index.ts
+++ libs/index.ts
@@ -6,17 +6,24 @@ export { Expression, isExpression } from './expression';
 export { IProperty, isIProperty } from './property';
 export { Parameter, isParameter } from './parameter';
 export { Guid } from './guid';
-export { GroupByBuilder, IGroupByAggregate, OperatorsEnum } from './group-by-builder';
 export {
-  IConnectionService, ConnectionService, config as DefaultConnectionServiceConfig,
+  GroupByBuilder,
+  IGroupByAggregate,
+  OperatorsEnum,
+} from './group-by-builder';
+export {
+  IConnectionService,
+  ConnectionService,
+  config as DefaultConnectionServiceConfig,
   IConnetionSettings,
 } from './connection-service';
 
-export {
-  IMutationResult, MutationResultStatus
-} from './mutation';
+export { IMutationResult, MutationResultStatus } from './mutation';
 
 import * as OData from './odata';
 import * as FilterBuilder from './filter-builder';
+import HttpResponse from './http-response';
 
 export { OData, FilterBuilder };
+
+export { HttpResponse };
